766d046c67ac91f141dc4dc8d01ba941ecd71974
viswanthkj <viswanth1995@gmail.com>
2024-04-11 12:52:47 +0530

    create product done

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -11,7 +11,7 @@ export default function MenuLayout() {
         options={{
           title: "Menu",
           headerRight: () => (
-            <Link href="/" asChild>
+            <Link href="/(admin)/menu/create" asChild>
               <Pressable>
                 {({ pressed }) => (
                   <FontAwesome
